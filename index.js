0c04610a5b737559d0e6bbe79f3edb9ca5bea67e
Edi Prayitno <cyberchrom91@gmail.com>
2018-08-06 23:00:21 +0700

    Mengenal Variabel di JS

---
+++ index.js
@@ -0,0 +1,9 @@
+// penulisan variabel pada java script ada 3 yaitu var let dan const
+var pesan = "Hello..";
+let pesan2 = "Mari Belajar ";
+const pesan3 = "Coding !!!";
+
+console.log(pesan)
+console.log(pesan2);
+console.log(pesan3);
+console.log(pesan + pesan2 + pesan3);
